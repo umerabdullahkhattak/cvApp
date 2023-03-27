20b46b85ce3f1fd38cd7a69db24d5cb660dc4ce7
umer <umer.a.khattak@gmail.com>
2023-03-28 03:35:45 +0500

    added fields in information part

--- src/Education.js
+++ src/Education.js
@@ -1,16 +1,73 @@
+// import React, { Component } from 'react';
+
+// class Education extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//         <div >
+//      <h1 className='education'>Education</h1>
+//      <hr/>
+//         </div>
+//     );
+//   }
+// }
+
+// export default Education;
 import React, { Component } from 'react';
 
 class Education extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      items: [],
+      showAddForm: false,
+      newItem: ''
+    };
+  }
+
+  handleAdd = () => {
+    this.setState({ showAddForm: true });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.newItem],
+      showAddForm: false,
+      newItem: ''
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ newItem: e.target.value });
   }
 
   render() {
+    const { items, showAddForm, newItem } = this.state;
+
     return (
-        <div >
-     <h1 className='education'>Education</h1>
-     <hr/>
-        </div>
+      <div>
+        <h1 className='education'>Education</h1>
+        <hr />
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        {!showAddForm && (
+          <button onClick={this.handleAdd}>Add</button>
+        )}
+        {showAddForm && (
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={newItem} onChange={this.handleChange} />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </div>
     );
   }
 }
--- src/Info.js
+++ src/Info.js
@@ -3,13 +3,92 @@ import React, { Component } from 'react';
 class Info extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      infoBits: {
+        name: '', 
+        email: '',
+        cellNo: ''
+      },
+      info: [],
+    }
+  }
+  handleAdd = () => {
+    this.setState({ showAddForm: true });
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      info: this.state.info.concat(this.state.infoBits),
+      infoBits: {
+        name: '', 
+        email: '',
+        cellNo: ''
+      }
+    });
+    
+  }
+  handleChangeName = (e) => {
+    this.setState({ 
+      infoBits: {
+        name: e.target.value,
+        email: this.state.infoBits.email,
+        cellNo: this.state.infoBits.cellNo,
+        
+      },
+                  
+                  });
+  } 
+  handleChangeEmail = (e) => {
+    this.setState({ 
+      infoBits: {
+        name: this.state.infoBits.name,
+        email: e.target.value,
+        cellNo: this.state.infoBits.cellNo,
+        
+      },
+                  
+                  });
+  }   
+  handleChangeCellNo = (e) => {
+    this.setState({ 
+      infoBits: {
+        name: this.state.infoBits.name,
+        email: this.state.infoBits.email,
+        cellNo: e.target.value,
+        
+      },
+      
+      });
+                  
+                  
+  } 
+
+  
   render() {
+    const { items, showAddForm, newItem,email,name,cellNo,infoBits,info } = this.state;
     return (
+      
         <div >
      <h1 className='info'>Information</h1>
+
      <hr/>
+     <ul className='infoList'>
+     {info.map((itemss, index) => (
+  <li key={index}>
+    <div> <h3>{itemss.name}</h3> <h4>{itemss.email}</h4><h4>{itemss.cellNo}</h4></div>
+    
+  </li>
+))}
+        </ul>
+     
+     <form onSubmit={this.handleSubmit} >
+        <input type="text"  value={infoBits.name}  onChange={this.handleChangeName} placeholder='Name' />
+        <input type="email" value={infoBits.email} onChange={this.handleChangeEmail} placeholder='Email' />
+        <input type="number" value={infoBits.cellNo} onChange={this.handleChangeCellNo} placeholder='Cell Number' />
+        <button type="submit">Submit</button>
+     </form>
+     
      </div>
     );
   }
